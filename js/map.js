838b2f74f1d3cda8b93ae36ba3d58bc6b48b62b7
dawgderps <elij@uw.edu>
2015-10-18 20:12:31 -0700

    context added

--- js/map.js
+++ js/map.js
@@ -1,76 +1,99 @@
-// Function to draw your map
-
-
-
+//author: Eli Jackson Oct 18,2015
+// Function to draw your map 
 var drawMap = function() {
-  // Provide your access token
-  //L.mapbox.accessToken = 'pk.eyJ1IjoiZWxpamoiLCJhIjoiY2lmd3psY2s0M2o5N3RmbTFlcXpxYngxMSJ9.yLK8vRQjqHB0lQ0vkA6vPw';
-  // Create a map in the div #map
-  //var map =L.mapbox.map('map', 'mapbox.streets');
-///
-
   // Create map and set view
- 	var map = L.map('container').setView([51.505, -0.09], 13);
+ 	var map = L.map('container').setView([39.5, -98.35], 4);
   // Create a tile layer variable using the appropriate url
-	
   	L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-    id: 'container',
+    id: 'mapbox.streets-satellite',
     accessToken: 'pk.eyJ1IjoiZWxpamoiLCJhIjoiY2lmd3psY2s0M2o5N3RmbTFlcXpxYngxMSJ9.yLK8vRQjqHB0lQ0vkA6vPw'
 }).addTo(map);
-
-  	getData;
-
-
-	//var layer = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png';
-  // Add the layer to your map
-   	//layer.addTo(map);
-  // Execute your function to get data
- 	//getData;
+  	// Add the layer to your map
+  	// Execute your function to get data
+  	getData(map);
 }
 
 // Function for getting data
-var getData = function() {
-	/*var data;
-	$.ajax({
-	     url:'https:..../json?year=2015&$limit=50000',
-	     type: "get",
-	     success:function(dat) {
-	       data = dat
-	       customBuild;
-	       // Do something with your data!
-	     }, 
-	     dataType:"json"
-	})
-*/
+var getData = function(map) {
+	var data;
+	$.get( "data/response.json", function( data ) {
+	  customBuild(data , map);  
+	});
   // Execute an AJAX request to get the data in data/response.js
-
-  
-
   // When your request is successful, call your customBuild function
-
 }
 
 // Loop through your data and add the appropriate layers and points
-var customBuild = function() {
-	
-	var layerName = new L.LayerGroup([]);
-	var circle = new L.circleMarker([latitude, longitude], options);
-	circle.addTo(layer);
-	circle.bindPopup(text)
-
-	// Be sure to add each layer to the map
-	L.control.layers(null,layers).addTo(map);
-
-	// Once layers are on the map, add a leaflet controller that shows/hides layers
-  
+var customBuild = function(data, map) {
+	var unknown = new L.LayerGroup([]);
+	var white = new L.LayerGroup([]);
+	var black = new L.LayerGroup([]);
+	var azn = new L.LayerGroup([]);
+	var nat = new L.LayerGroup([]);
+	var island = new L.LayerGroup([]);
+	var unarmedWhite = 0;
+	var armedWhite = 0;
+	var armedNon = 0;
+	var unarmedNon = 0;
+	data.map(function(value){
+		if (value.Weapon == 'Unarmed'){
+			if(value.Race != 'White'){
+				unarmedNon += 1;
+			}else{
+				unarmedWhite += 1;
+			}
+		} else {
+			if(value.Race != 'White'){
+				armedNon += 1;
+			}else{
+				armedWhite += 1;
+			}
+		}
+		if (value["Hit or Killed?"] == 'Killed'){
+			var circle = new L.circleMarker([value.lat, value.lng],{
+			color : 'red'
+			});
+			circle.setRadius(7);
+		}else{
+			var circle = new L.circleMarker([value.lat, value.lng],{
+			color : 'black'
+			});
+			circle.setRadius(5);
+		}
+		circle.bindPopup(value.Summary + " <a href ="+value["Source Link"] +">(link)</a>");
+		if (value.Race =='Unknown'){
+			circle.addTo(unknown);
+		}
+		if (value.Race == 'White'){
+			circle.addTo(white);
+		}
+		if (value.Race == 'Black or African American'){
+			circle.addTo(black);
+		}
+		if (value.Race == 'Asian'){
+			circle.addTo(azn);
+		}
+		if (value.Race == 'American Indian or Alaska Native'){
+			circle.addTo(nat);
+		}
+		if (value.Race == 'Native Hawaiian or Other Pacific Islander'){
+			circle.addTo(island);
+		}	
+	})
+	var layers = {
+		"Unknown" : unknown,
+		"White" : white,
+		"Black" : black,
+		"Asian" : azn,
+		"American Indian or Alaska Native" : nat,
+		"Native Hawaiian or Other Pacific Islander" : island
+	};
+	L.control.layers(null,layers,{
+		collapsed : false
+	}).addTo(map);
+	$('#x2y2').html(unarmedWhite);
+	$('#x1y2').html(armedWhite);
+	$('#x1y1').html(armedNon);
+	$('#x2y1').html(unarmedNon);
 }
-
-
-//https://api.mapbox.com/v4/{resource}.json?access_token=pk.eyJ1IjoiZWxpamoiLCJhIjoiY2lmd3psY2s0M2o5N3RmbTFlcXpxYngxMSJ9.yLK8vRQjqHB0lQ0vkA6vPw
-
-var buildTable = function (){
-
-}
-
-
